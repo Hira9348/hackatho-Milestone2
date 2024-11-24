5d8c4a1148824678d23da0d14e879d789e89c5bb
Hira9348 <hiraimi608@gmail.com>
2024-11-24 07:08:34 -0800

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-16 py-6 text-center bg-blue-800'>
+    <div className='mt-16 py-6 text-center bg-blue-800 '>
         @ Hira Imtiaz | All rights reserved 2024.
     </div>
   )
